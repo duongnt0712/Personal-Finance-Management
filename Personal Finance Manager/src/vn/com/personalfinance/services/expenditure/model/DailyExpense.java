122f22d8ca3df04d13575b6b529494e8d3e0a76c
ADMIN <ADMIN@LAPTOP-Q5ETGIH4>
2021-04-12 12:54:01 +0700

    Update: .gitignore

--- Personal Finance Manager/src/vn/com/personalfinance/services/expenditure/model/DailyExpense.java
+++ Personal Finance Manager/src/vn/com/personalfinance/services/expenditure/model/DailyExpense.java
@@ -25,208 +25,192 @@ import vn.com.personalfinance.services.expenditure.report.DailyExpenseByDateRepo
 @DClass(schema="personalfinancemanagement")
 public abstract class DailyExpense {
 	
-		public static final String D_id = "id";
-		public static final String D_amount = "amount";
-		public static final String D_date = "date";
-		public static final String D_category = "category";
-		public static final String D_account = "account";
-		public static final String D_description = "description";
-		public static final String D_rptDailyExpenseByCategory = "rptDailyExpenseByCategory";
-		public static final String D_rptDailyExpenseByDate = "rptDailyExpenseByDate";
+	public static final String D_id = "id";
+	public static final String D_amount = "amount";
+	public static final String D_date = "date";
+	public static final String D_category = "category";
+	public static final String D_account = "account";
+	public static final String D_description = "description";
+	public static final String D_rptDailyExpenseByCategory = "rptDailyExpenseByCategory";
+	public static final String D_rptDailyExpenseByDate = "rptDailyExpenseByDate";
 
-		// attributes of daily expense
-		@DAttr(name = D_id, id = true, type = Type.String, auto = true, length = 6, mutable = false, optional = false)
-		private String id;
-		// static variable to keep track of account id
-		public static int idCounter = 0;
-		
-		@DAttr(name = D_amount, type = Type.Double, length = 15, optional = false)
-		private double amount;
-			
-		@DAttr(name = D_date, type = Type.Date, length = 15, optional = false) 
-		private Date date;
-		
-		@DAttr(name =D_category, type = Type.Domain, optional = false)
-		@DAssoc(ascName = "category-has-dailyExpense", role = "category",
-		ascType = AssocType.One2Many, endType = AssocEndType.Many,
-		associate = @Associate(type = Category.class, cardMin = 1, cardMax = 1),
-		dependsOn=true)
-		private Category category;
-		
-		@DAttr(name =D_account, type = Type.Domain,  optional = false) 
-		@DAssoc(ascName = "account-has-dailyExpense", role = "account",
-		ascType = AssocType.One2Many, endType = AssocEndType.Many,
-		associate = @Associate(type = Account.class, cardMin = 1, cardMax = 1),
-		dependsOn=true)
-		private Account account;
-		
-		@DAttr(name = D_description, type = Type.String, length = 30)
-		private String description;
-		
-		@DAttr(name = D_rptDailyExpenseByCategory, type = Type.Domain,
-		serialisable = false, virtual = true)
-		private DailyExpenseByCategoryReport rptDailyExpenseByCategory;
-		
-		@DAttr(name = D_rptDailyExpenseByDate, type = Type.Domain,
-		serialisable = false, virtual = true)
-		private DailyExpenseByDateReport rptDailyExpenseByDate;
+	// attributes of daily expense
+	@DAttr(name = D_id, id = true, type = Type.String, auto = true, length = 6, mutable = false, optional = false)
+	private String id;
+	// static variable to keep track of account id
+	public static int idCounter = 0;
 
-		//constructor methods
-		@DOpt(type=DOpt.Type.ObjectFormConstructor)
-		@DOpt(type=DOpt.Type.RequiredConstructor)
-		protected DailyExpense(@AttrRef("amount") Double amount,
-				@AttrRef("date") Date date,
-				@AttrRef("category") Category category,
-				@AttrRef("account") Account account,
-				@AttrRef("description") String description
-				) {
-			this(null, amount, date, category, account, description);
-		}
-			
-		// a shared constructor that is invoked by other constructors
-		@DOpt(type=DOpt.Type.DataSourceConstructor)
-		protected DailyExpense (String id, Double amount, Date date, Category category,Account account, String description)
-			 {
-			// generate an id
-			this.id = nextID(id);
-			    
-			// assign other values
-			this.amount = amount;
-			this.date = date;
-			this.category = category;
-			this.account = account;
-			this.description = description;
-			
-			computeNewBalance();
-		}
-		// getter and setter method
+	@DAttr(name = D_amount, type = Type.Double, length = 15, optional = false)
+	private double amount;
 
-		public String getId() {
-			return id;
-		}
+	@DAttr(name = D_date, type = Type.Date, length = 15, optional = false)
+	private Date date;
 
-		
+	@DAttr(name = D_category, type = Type.Domain, optional = false)
+	@DAssoc(ascName = "category-has-dailyExpense", role = "category", ascType = AssocType.One2Many, endType = AssocEndType.Many, associate = @Associate(type = Category.class, cardMin = 1, cardMax = 1), dependsOn = true)
+	private Category category;
 
-		public double getAmount() {
-			return amount;
-		}
+	@DAttr(name = D_account, type = Type.Domain, optional = false)
+	@DAssoc(ascName = "account-has-dailyExpense", role = "account", ascType = AssocType.One2Many, endType = AssocEndType.Many, associate = @Associate(type = Account.class, cardMin = 1, cardMax = 1), dependsOn = true)
+	private Account account;
 
-		public void setAmount(double amount) {
-			this.amount = amount;
-		}
+	@DAttr(name = D_description, type = Type.String, length = 30)
+	private String description;
 
-		public Date getDate() {
-			return date;
-		}
+	@DAttr(name = D_rptDailyExpenseByCategory, type = Type.Domain, serialisable = false, virtual = true)
+	private DailyExpenseByCategoryReport rptDailyExpenseByCategory;
 
-		public void setDate(Date date) {
-			this.date = date;
-		}
+	@DAttr(name = D_rptDailyExpenseByDate, type = Type.Domain, serialisable = false, virtual = true)
+	private DailyExpenseByDateReport rptDailyExpenseByDate;
 
-		public Category getCategory() {
-			return category;
-		}
+	// constructor methods
+	@DOpt(type = DOpt.Type.ObjectFormConstructor)
+	@DOpt(type = DOpt.Type.RequiredConstructor)
+	protected DailyExpense(@AttrRef("amount") Double amount, @AttrRef("date") Date date,
+			@AttrRef("category") Category category, @AttrRef("account") Account account,
+			@AttrRef("description") String description) {
+		this(null, amount, date, category, account, description);
+	}
 
-		public void setCategory(Category category) {
-			this.category = category;
-		}
+	// a shared constructor that is invoked by other constructors
+	@DOpt(type = DOpt.Type.DataSourceConstructor)
+	protected DailyExpense(String id, Double amount, Date date, Category category, Account account,
+			String description) {
+		// generate an id
+		this.id = nextID(id);
 
-		public Account getAccount() {
-			return account;
-		}
+		// assign other values
+		this.amount = amount;
+		this.date = date;
+		this.category = category;
+		this.account = account;
+		this.description = description;
 
-		public void setAccount(Account account) {
-			this.account = account;
-		}
-		
-		public String getDescription() {
-			return description;
-		}
+		computeNewBalance();
+	}
+	// getter and setter method
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		
-		public DailyExpenseByCategoryReport getRptDailyExpenseByCategory() {
-			return rptDailyExpenseByCategory;
-		}
-		
-		public DailyExpenseByDateReport getRptDailyExpenseByDate() {
-			return rptDailyExpenseByDate;
-		}
+	public String getId() {
+		return id;
+	}
 
-		public abstract String nextID(String currID) ;
-		public abstract void computeNewBalance();
-		@Override
-		public String toString() {
-			return toString(true);
-		}
-		
-		/**
-		 * @effects returns <code>Savings(id,code,amount,name,purpose,startDate,monthlyDuration)</code>.
-		 */
-		public String toString(boolean full) {
-			if (full)
-				return "DailyExpense(" + id + ","  + amount + "," + date + ","   + category + "," +  account +")";
-			else
-				return "DailyExpense(" + id + ")";
-		}
+	public double getAmount() {
+		return amount;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + Integer.parseInt(id.substring(1));
-			return result;
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public DailyExpenseByCategoryReport getRptDailyExpenseByCategory() {
+		return rptDailyExpenseByCategory;
+	}
+
+	public DailyExpenseByDateReport getRptDailyExpenseByDate() {
+		return rptDailyExpenseByDate;
+	}
+
+	public abstract String nextID(String currID);
+
+	public abstract void computeNewBalance();
+
+	@Override
+	public String toString() {
+		return toString(true);
+	}
+
+	/**
+	 * @effects returns
+	 *          <code>Savings(id,code,amount,name,purpose,startDate,monthlyDuration)</code>.
+	 */
+	public String toString(boolean full) {
+		if (full)
+			return "DailyExpense(" + id + "," + amount + "," + date + "," + category + "," + account + ")";
+		else
+			return "DailyExpense(" + id + ")";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Integer.parseInt(id.substring(1));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DailyExpense other = (DailyExpense) obj;
+
+		if (!(id.equals(other.id))) {
+			return false;
 		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			DailyExpense other = (DailyExpense) obj;
-			
-			
-			 if (!(id.equals(other.id))) {
-				return false;	
-			 }	
-			 return true;		
-		}
-		/**
-		 * @requires minVal != null /\ maxVal != null
-		 * @effects update the auto-generated value of attribute <tt>attrib</tt>,
-		 *          specified for <tt>derivingValue</tt>, using <tt>minVal, maxVal</tt>
-		 */
-		@DOpt(type = DOpt.Type.AutoAttributeValueSynchroniser)
-		public static void updateAutoGeneratedValue(DAttr attrib, Tuple derivingValue, Object minVal, Object maxVal)
-				throws ConstraintViolationException {
-			if (minVal != null && maxVal != null) {
-				// check the right attribute
-				  if (attrib.name().equals("id")) {
-			        String maxID = (String) maxVal;
-			        
-			        try {
-			          int maxIDNum = Integer.parseInt(maxID.substring(1));
-			          
-			         
-			          if(maxIDNum>idCounter) {
-			        	  idCounter= maxIDNum;
-			          }
-			          
-			          
-			          
-			        } catch (RuntimeException e) {
-			          throw new ConstraintViolationException(
-			              ConstraintViolationException.Code.INVALID_VALUE, e, new Object[] {maxID});
-			        }
+		return true;
+	}
+
+	/**
+	 * @requires minVal != null /\ maxVal != null
+	 * @effects update the auto-generated value of attribute <tt>attrib</tt>,
+	 *          specified for <tt>derivingValue</tt>, using <tt>minVal, maxVal</tt>
+	 */
+	@DOpt(type = DOpt.Type.AutoAttributeValueSynchroniser)
+	public static void updateAutoGeneratedValue(DAttr attrib, Tuple derivingValue, Object minVal, Object maxVal)
+			throws ConstraintViolationException {
+		if (minVal != null && maxVal != null) {
+			// check the right attribute
+			if (attrib.name().equals("id")) {
+				String maxID = (String) maxVal;
+
+				try {
+					int maxIDNum = Integer.parseInt(maxID.substring(1));
+
+					if (maxIDNum > idCounter) {
+						idCounter = maxIDNum;
+					}
+
+				} catch (RuntimeException e) {
+					throw new ConstraintViolationException(ConstraintViolationException.Code.INVALID_VALUE, e,
+							new Object[] { maxID });
 				}
-		
-}
+			}
 		}
+	}
 }
-		
-
